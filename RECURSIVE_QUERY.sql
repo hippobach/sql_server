bae4cf7583651af32400294fbdfb8e74cf8dddd5
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-09-23 09:55:15 +0700

    recursive query in sql server

---
+++ RECURSIVE_QUERY.sql
@@ -0,0 +1,33 @@
+﻿-- Truy vấn sử dụng CTE
+WITH FIBO(prev_n, n) AS (
+	-- Khởi tạo
+	SELECT
+		0 AS prev_n,
+		1 AS n
+	UNION ALL
+	-- Đệ quy
+	SELECT 
+		n AS prev_n,
+		prev_n + n AS n
+	FROM FIBO
+)
+
+SELECT * FROM FIBO
+OPTION (MAXRECURSION 5)
+
+-- Tính giai thừa
+WITH giaiThua(stt, giaiThuaX) AS (
+	-- Khởi tạo
+	SELECT
+		1 AS stt,
+		1 AS giaiThuaX
+	UNION ALL
+	-- Đệ quy
+	SELECT 
+		stt+1 AS stt,
+		(stt+1)*giaiThuaX AS giaiThuaX
+	FROM giaiThua
+)
+
+SELECT * FROM giaiThua
+OPTION (MAXRECURSION 5)
