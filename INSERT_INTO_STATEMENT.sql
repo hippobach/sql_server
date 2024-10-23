3e33f0e1f4155571ed0b6e05c87edc0174e12e2e
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-10-23 23:48:49 +0700

    insert into statement in sql server

---
+++ INSERT_INTO_STATEMENT.sql
@@ -0,0 +1,14 @@
+﻿-- Thêm một khách hàng mới
+INSERT INTO [dbo].[Customers] ([CustomerID], [CompanyName], [ContactName], [Phone])
+VALUES ('KH123', 'BachNguyen', 'Hippo', '0345434221')
+
+-- Thêm nhiều khách hàng mới cùng một lúc
+INSERT INTO [dbo].[Customers] ([CustomerID], [CompanyName], [ContactName], [Phone])
+VALUES 
+	('KH124', 'James', 'Reccee', '0345434221'),
+	('KH125', 'HuyenNguyen', 'Huyen', '0989770997'),
+	('KH126', 'TungNguyen', 'Tung', '0385433174')
+
+-- Thêm một bản ghi vào bảng Suppliers
+INSERT INTO [dbo].[Suppliers] ([CompanyName], [ContactName], [ContactTitle], [Address], [City], [Region], [PostalCode], [Country], [Phone], [Fax], [HomePage])
+VALUES ('BlueOC', 'John Smith', 'Sales Managers', 'Vin Ocean Park', 'New York',  'NY', '10001', 'USA', '555-555-5555', '555-555-5556', 'http://www.blueoc.com')
